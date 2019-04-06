8ebfbd1fc70d555a15654b8b0430f7c22865394d
Renu Punjabi <rpunjabi12@gmail.com>
2019-04-06 12:36:01 -0700

    restructured the folders

---
+++ Weather/Scenes/Weather/Views/WeatherCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCell.swift
+//  Weather
+//
+//  Created by Renu Punjabi on 4/6/19.
+//  Copyright © 2019 Renu Punjabi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class WeatherCell: UITableViewCell {
+    
+    func configureCell() {
+        
+    }
+}
